9ec6e8e0f31e24f6f60cc757d70e430a10045bfb
Lance Whatley <whatl3y@gmail.com>
2021-04-24 10:53:47 -0400

    Increase possible num chars for random string generation

--- src/libs/Random.ts
+++ src/libs/Random.ts
@@ -2,7 +2,7 @@ export default {
   chars:
     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890`~!@#$%^&*()_+-=\'";:,./<>?[]{}|',
 
-  string(length: number = Math.floor(Math.random() * 100 + 1)) {
+  string(length: number = Math.floor(Math.random() * 1e4) + 1) {
     return new Array(length)
       .fill(0)
       .map((_, i) => this.chars[Math.floor(Math.random() * this.chars.length)])
